baa7d857e0cf8d4b4f04df28b82bd0b351ddf798
Jan Kolkmeier <jankolkmeier@gmail.com>
2016-08-19 21:14:03 +0200

    Example Scripts for moving sphere

---
+++ ExampleScene/Scripts/MoveGreenSphere.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGreenSphere : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (0.75f + Mathf.Sin(Time.time * speed) * 1.5f, 1.5f + Mathf.Cos(Time.time * speed) * 1f, 0.5f);
+	}
+}
