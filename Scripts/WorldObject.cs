9043852d3b59a7ec563562ab5d768fba550369e0
Jan Kolkmeier <jankolkmeier@gmail.com>
2016-08-19 21:13:12 +0200

    Attach WorldObject.cs to any gameobject to have it available from BML by its name

---
+++ Scripts/WorldObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ASAP {
+
+	public class WorldObject : MonoBehaviour {
+
+		private VJoint vjoint;
+
+		// Use this for initialization
+		void Start () {
+			vjoint = new VJoint (transform.name, transform.position, transform.rotation);
+			FindObjectOfType<ASAPManager>().OnWorldObjectInitialized(vjoint);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			//if (vjoint.position.Equals (transform.position)) {
+			// Todo: only transmit if changed? use a flag for "moved" ?
+			//}
+			vjoint.position = transform.position;
+			vjoint.rotation = transform.rotation;
+
+		}
+	}
+
+}
