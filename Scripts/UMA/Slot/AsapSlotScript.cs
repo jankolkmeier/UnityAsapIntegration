2afa8855d1a6cba7cfbda7ae7163ecc267393232
Jan Kolkmeier <jankolkmeier@gmail.com>
2016-11-19 13:58:09 +0100

    Fixed wierd condition where agent would be configured too often

--- Scripts/UMA/Slot/AsapSlotScript.cs
+++ Scripts/UMA/Slot/AsapSlotScript.cs
@@ -7,21 +7,24 @@ using ASAP;
 
 public class AsapSlotScript : MonoBehaviour {
 
-//    bool isConfigured = false;
+    bool isConfigured = false;
 
-    public void OnCharacterBegun(UMAData umaData) {}
+    public void OnCharacterBegun(UMAData umaData) {
+        isConfigured = false;
+    }
 
 
     public void OnDnaApplied(UMAData umaData) {}
 
     public void OnCharacterCompleted(UMAData umaData) {
-        //if (isConfigured) return; // TODO: we might consider notifiying asap?
         ASAPAgent_UMA asapAgent = umaData.gameObject.GetComponentInChildren<ASAPAgent_UMA>();
         if (asapAgent == null) {
             asapAgent = umaData.gameObject.AddComponent<ASAPAgent_UMA>();
             // need HumanoidRoot?/
         }
-        asapAgent.UMAConfigure(umaData);
-        //isConfigured = true;
+        if (!isConfigured) {
+            isConfigured = true;
+            asapAgent.UMAConfigure(umaData);
+        }
     }
 }
